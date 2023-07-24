384fba34498465abad208fd4e8f06196b194fe10
Hari-Bombon <haripriya14022003@gmail.com>
2023-07-24 10:59:16 +0530

    fix the code

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -1,68 +1,64 @@
-import { Button } from '@material-ui/core';
-import Modal from '@material-ui/core/Modal';
-import { makeStyles } from '@material-ui/core/styles';
 import React, { useEffect, useState } from 'react';
+import { Button, default as Input, default as Modal, makeStyles } from 'react-icons';
 import './App.css';
+import ImageUpload from './ImageUpload';
 import Post from './Post';
 import { auth, db } from './firebase';
-import ImageUpload from './ImageUpload';
 
 function getModalStyle() {
-  const top = 50;
-  const left = 50 ;
-
-  return{
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}% -${left}%)`,
-  };
-}
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow:theme.shadows[5],
-    padding: theme.spacing(2,4,3),
+    const top = 50;
+    const left = 50 ;
+  
+    return{
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}% -${left}%)`,
+    };
   }
-}));
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow:theme.shadows[5],
+      padding: theme.spacing(2,4,3),
+    }
+  }));
+  const signUp =(event) =>{
 
-const signUp = (event) => {
-
-}
+  }
 
 function App() {
-  const classes = useStyles();
-  const [modalStyle] = useState(getModalStyle);
-  const[post, setPosts] =useState ([]);
-  const [openSignIn , setOpenSignIn] = useState('');
-  const [open, setOpen] = useState(false);
-  const [username , setUsername] =useState('');
-  const [password, setPassword] =useState ('');
-  const [email , setEmail] =useState('');
-  const [user , setUser] = useState(null);
+    const classes = useStyles();
+    const [getModalStyle] = useState(getModalStyle);
+    const[post, setPosts] =useState ([]);
+    const [openSignIn , setOpenSignIn] = useState('');
+    const [open, setOpen] = useState(false);
+    const [username , setUsername] =useState('');
+    const [password, setPassword] =useState ('');
+    const [email , setEmail] =useState('');
+    const [user , setUser] = useState(null);
   
-  useEffect(()=> {
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        //user has logged in..
-        console.log(authUser);
-        setUser(authUser);
-      }else{
-        //user has logged out..
-        setUser(null);
-      }
-    })
-    return() => {
-      //perform some cleanup actions
-      unsubscribe();
-    }
-    
-  },[user , username]);
-
-
-  // useEffect ->runs a piece of code based on specific condition
+    useEffect(()=> {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+          if (authUser) {
+            //user has logged in..
+            console.log(authUser);
+            setUser(authUser);
+          }else{
+            //user has logged out..
+            setUser(null);
+          }
+        })   
+        return() => {
+            //perform some cleanup actions
+            unsubscribe();
+          }
+          
+        },[user , username]);
+       
+          // useEffect ->runs a piece of code based on specific condition
   useEffect (() => {
     //this is where the code runs
 db.collection('posts').onSnapshot (snapshot=>{
@@ -71,99 +67,89 @@ setPosts(snapshot.docs.map(doc => ({
   id: doc.id,
   posts:doc.data()
 })));
+
 })
 },[]);
+
 const signUp = (event) => {
-  event.preventDefault();
-  
-  
-  auth
-  .createUserWithEmailAndPassword(email , password)
-  .then((authUser) =>{
-    return authUser.user.updateProfile({
-      displayName: username
+    event.preventDefault();
+    
+    
+    auth
+    .createUserWithEmailAndPassword(email , password)
+    .then((authUser) =>{
+      return authUser.user.updateProfile({
+        displayName: username
     })
-  })
-  .catch((error) => alert(error.message));
+    })
+    .catch((error) => alert(error.message));
   setOpen(false);
 }
+
 const signIn = (event) => {
-  event.preventDefault();
-
-  auth.signInEmailAndPassword(email, password)
-  .catch((error) => alert(error.message))
+    event.preventDefault();
   
-  setOpenSignIn(false);
-}
-
+    auth.signInEmailAndPassword(email, password)
+    .catch((error) => alert(error.message))
+    
+    setOpenSignIn(false);
+  }
   return (
     <div className="App">
-  <ImageUpload />
+      <ImageUpload />
       <Modal
         open={openSignIn}
         onClose={() => setOpenSignIn(false)}
       >
         <div style={modalStyle} className={classes.paper}>
-          <form className="app_signup"> 
-          <center>
-          <img
-            className="app_headerImage"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
-            alt=""
-        />
-          </center>
-        {user ? (
-        <Button onClick={() => auth.signOut}>Logout</Button>
-        ): (
-          <div className="app_loginContainer">
-        <Button onClick={() => setOpen(true)}>Sign In</Button>
-        <Button onClick={() => setOpen(true)}>SignUp</Button>
-        </div>
-        )}
-        
-        <Input
-          type="text"
-          placeholder="username"
-          value={username}
-          onChange={(e) => setEmail(e.target.value)}
-          />
-        <Input
-        placeholder="email"
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-        placeholder="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={signUp}>Sign Up</Button>
-
+          <form className="app_signup">
+            <center>
+              <img
+                className="app_headerImage"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+                alt=""
+              />
+            </center>
+            {user ? (
+              <button onClick={() => auth.signOut()}>Logout</button>
+            ) : (
+              <div className="app_loginContainer">
+                <Button onClick={() => setOpen(true)}>Sign In</Button>
+                <Button onClick={() => setOpen(true)}>SignUp</Button>
+              </div>
+            )}
+            <Input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signUp}>Sign Up</Button>
           </form>
-          
-    
-    </div>
-  </Modal>
-
-    <Button type="submit" onClick={() => setOpen(true)}> Sign Up</Button>
+        </div>
+      </Modal>
+      <Button type="submit" onClick={() => setOpen(true)}> Sign Up</Button>
     {
       post.map (({id, post}) =>(
         <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
       ))
     }
-
-    <Post username="Hy" caption="minions" imageUrl="https://tse1.mm.bing.net/th?id=OIP.3yqBVngk9xy4ZcEh5Zq3RQHaGW&pid=Api&P=0&h=180"/>
+        <Post username="Hy" caption="minions" imageUrl="https://tse1.mm.bing.net/th?id=OIP.3yqBVngk9xy4ZcEh5Zq3RQHaGW&pid=Api&P=0&h=180"/>
     <Post username="remo" caption="siavas" imageUrl="https://tse1.mm.bing.net/th?id=OIP.NfcnKBmZD3OrLNSYdSKbUgHaJ4&pid=Api&P=0&h=180"/>
     <Post username="cheez" caption="kakasuki" imageUrl="https://tse3.mm.bing.net/th?id=OIP.LlDtcenlPDhLGzkCyE03xAHaIo&pid=Api&P=0&h=180"/>
-  
-  
-    
+
     {/*posts */}
      {/*posts */}
+
+
     </div>
   );
-}
 
 export default App;
+
--- insta-clone/src/ImageUpload.js
+++ insta-clone/src/ImageUpload.js
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
+import { FaUpload } from "react-icons/fa";
 
 function ImageUpload() {
-    const[caption, setCaption] = useState('null');
-    const [progress, setProgress] = useState(0);
-    const [caption, setCaption] = useState('');
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [caption, setCaption] = useState('');
 
-    const handleChange= (e) => {
-        if (e.target.files[0]){
-            setImage(e.target.files[0]);
-        }
-    };
-    return{
-        <div>
-
-        {/* i want to have..*/}
-         {/* caption input*/}
-          {/* file picker*/}
-           {/*post button */}
-                <input type="text" placeholder='Enter a caption' 
-            onChange={event =>setCaption(event.target.value)}value={}/>
-            <input type="file" onChange={handleChange} />
-            <Button onClick={handleUpload}>
-            Upload
-           </Button>
-    </div>
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
     }
+  };
+
+  const handleUpload = () => {
+    // Implement the logic to handle the upload process.
+    // Send the selected file and caption to the server.
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter a caption"
+        onChange={(event) => setCaption(event.target.value)}
+        value={caption}
+      />
+      <input type="file" onChange={handleChange} />
+      <button onClick={handleUpload}>
+        <FaUpload /> Upload
+      </button>
+    </div>
+  );
 }
 
-export default ImageUpload
+export default ImageUpload;
